4831a48551bf6cf4542581611794526be9106212
Nadav Abrahami <nadavwix@wix.com>
2017-07-05 16:23:33 +0300

    added mixin orchestrator (#6)
    * mixin orchestrator draft 1
    
    * class-decor with limited functionality
    
    * documentation

---
+++ docs/mixins.md
@@ -0,0 +1,101 @@
+# mixins
+
+mixins allow modifying the behavior of classes/components
+
+all mixins can be flagged disabled for a specific inheriting class
+
+Each mixin provides:
+* decorator to add implementation
+* typescript interface to implement
+* props interface
+
+# how to use
+
+```tsx
+import {globalId,globalIdInterface, globalIdProps}
+
+@globalId
+class MyComp extends React.Component<globalIdProps & MyProps,any> implements globalIdInterface{
+    getGlobalId(...rest):string{};
+    render(){
+        <div>
+            <input id="nameInput"></input>
+            <label forHtml="nameInput"></label>
+        </div>
+    }
+}
+
+React.render(<MyComp id="App"></MyComp>)
+
+//will render:
+
+<div>
+    <input id="App-nameInput"></input>
+    <label forHtml="App-nameInput"></label>
+</div>
+```
+
+## available mixins:
+
+[global-id](https://github.com/wix/react-bases/blob/master/docs/global-id.md)
+
+*Easily generate namespaced global DOM ID's that can be used in label forHtml property and more*
+
+[props-injection](https://github.com/wix/react-bases/blob/master/docs/props-injection.md)
+
+*allows injecting project resources directly into internal component props*
+
+[copy-data-attributes](https://github.com/wix/react-bases/blob/master/docs/copy-data-attributes.md)
+
+*merge all data-\* attributes to the component root element*
+
+[copy-aria](https://github.com/wix/react-bases/blob/master/docs/copy-aria.md)
+
+*merge all aria attributes to the component root element*
+
+[merge-class-names](https://github.com/wix/react-bases/blob/master/docs/merge-class-names.md)
+
+*adds className prop to the component root element className*
+
+[merge-inline-style](https://github.com/wix/react-bases/blob/master/docs/merge-inline-style.md)
+
+*merge inline style to the component root element style attribute*
+
+[langauges](https://github.com/wix/react-bases/blob/master/docs/langauges.md)
+
+*allows easily using langauge files*
+
+[disposable](https://github.com/wix/react-bases/blob/master/docs/disposable.md)
+
+*API to register for component unmount for destruct - centrelized dispose hook for mixins*
+
+### events
+
+[mouse-toucher](https://github.com/wix/react-bases/blob/master/docs/mouse-toucher.md)
+
+*automatically listens to the corresponding touch event when listening to a mouse event*
+
+[safe-global-events](https://github.com/wix/react-bases/blob/master/docs/safe-global-events.md)
+
+*adds addGlobalEvent, setInterval, setTimout, requestAnimationFrame to the component prototype. added event listeners are safe to use in server and dispose automatically when component unmounts*
+
+[on-click-outside](https://github.com/wix/react-bases/blob/master/docs/on-click-outside.md)
+
+*adds onClickOutside to prototype and props*
+
+
+
+
+
+## 1st stage unsolved:
+
+controled / uncontroled
+
+we're looking for a mixin or pattern to make our components all controlled/uncontrolled according to the users choice
+
+
+## 2nd stage:
+
+dom ref
+
+allows reference between sibling components (used for position floating UI extensions)
---
+++ src/mixin-orchestrator.ts
@@ -0,0 +1,110 @@
+import {ReactConstructor} from './utils/types';
+import _map = require('lodash/map');
+import _reduce = require('lodash/reduce');
+import _forEachRight = require('lodash/forEachRight');
+export type lifeCycleHookModifier = 'before' | 'after' //| 'around';;
+export type lifeCycleRunnner<T> = (target:T)=>void;
+export type lifeCycleHookName =  keyof lifeCycleHooks<any>;
+export interface lifeCycleHook<T>{
+    before?:lifeCycleRunnner<T>[];
+    after?:lifeCycleRunnner<T>[];
+}
+
+
+export interface lifeCycleHooks<T>{
+    render?:lifeCycleHook<T>;
+    componentDidMount?:lifeCycleHook<T>;
+    componentWillReceiveProps?:lifeCycleHook<T>;
+    shouldComponentUpdate?:lifeCycleHook<T>;
+    componentWillUpdate?:lifeCycleHook<T>;
+    componentDidUpdate?:lifeCycleHook<T>;
+    componentWillUnmount?:lifeCycleHook<T>;
+}
+const lifecycleHooks:lifeCycleHookName[] = ['render','componentDidMount','componentWillReceiveProps','shouldComponentUpdate','componentWillUpdate','componentDidUpdate','componentWillUnmount'];
+const orchastratorSymbol:string = 'orchastratorSymbol'
+
+export interface orchestratorMeta<T>{
+    constructorHooks?:lifeCycleRunnner<T>[];
+    lifeCycleHooks?:lifeCycleHooks<T>;
+}
+
+export class WithOrchestratorMetaCls<T>{
+    orchastratorSymbol?:orchestratorMeta<T>
+}
+
+export type WithOrchestratorMeta<T> = T & WithOrchestratorMetaCls<T>;
+
+const runLifeCycleHooks = {
+    ..._reduce(lifecycleHooks,(accum:{[key:string]:Function},hookName:lifeCycleHookName)=>{
+        accum[hookName] = function(...args:any[]){
+            const hooks = (this.constructor as WithOrchestratorMeta<any>).orchastratorSymbol.lifeCycleHooks[hookName];
+            if(hooks.before){
+                hooks.before.forEach(function(this:any,hook:Function){
+                    hook(this,...args);
+                });
+            }
+            const res = (this as any)['_original_'+hookName](...args);
+
+            if(hooks.after){
+                _forEachRight(hooks.after,function(this:any,hook:Function){
+                    hook(this,...args);
+                });
+            }
+            return res;
+        }
+        return accum
+    },{})
+
+}
+
+
+function activateMixins<T>(target:any){
+    if(target.constructor && target.constructor.orchastratorSymbol){
+        target.constructor.orchastratorSymbol.constructorHooks && target.constructor.orchastratorSymbol.constructorHooks.forEach((cb:lifeCycleRunnner<any>)=>{
+            cb(target);
+        });
+        _map(target.constructor.orchastratorSymbol.lifeCycleHooks,(lifeCycleHook:lifeCycleHook<T>,lifeCycleName:lifeCycleHookName)=>{
+            target['_original_'+lifeCycleName] = target[lifeCycleName];
+            target[lifeCycleName] = (runLifeCycleHooks as any)[lifeCycleName];
+        });
+    }
+
+}
+
+export function orchastrated<C extends ReactConstructor<{}>>(cls:C):C{
+    if(!(cls as any).isOrchastrated){
+        return class extends cls {
+            static isOrchastrated:boolean = true;
+            constructor(...args:any[]){
+                super(...args);
+                activateMixins((this as any) as WithOrchestratorMeta<C>)
+            }
+        };
+    }
+    return cls;
+}
+
+export function registerForConstructor<T,TE  extends WithOrchestratorMeta<T> & T>(target:T,cb:lifeCycleRunnner<TE>):TE{
+    const targetE = (target as any) as TE;
+    targetE.orchastratorSymbol = targetE.orchastratorSymbol || {
+        constructorHooks:[]
+    };
+    targetE.orchastratorSymbol.constructorHooks = targetE.orchastratorSymbol.constructorHooks || [];
+    targetE.orchastratorSymbol.constructorHooks.push(cb);
+    return targetE;
+}
+
+export function registerLifeCycle<T,TE  extends WithOrchestratorMeta<T> & T>(
+                target:T,
+                lifeCycleHookModifier:lifeCycleHookModifier,
+                lifeCycleHook:lifeCycleHookName,
+                cb:lifeCycleRunnner<TE>){
+    const targetE = (target as any) as TE;
+    targetE.orchastratorSymbol = targetE.orchastratorSymbol || {
+        lifeCycleHooks:{}
+    };
+    targetE.orchastratorSymbol.lifeCycleHooks![lifeCycleHook] = targetE.orchastratorSymbol.lifeCycleHooks![lifeCycleHook] || {};
+    targetE.orchastratorSymbol.lifeCycleHooks![lifeCycleHook]![lifeCycleHookModifier] = targetE.orchastratorSymbol.lifeCycleHooks![lifeCycleHook]![lifeCycleHookModifier] || [];
+    targetE.orchastratorSymbol.lifeCycleHooks![lifeCycleHook]![lifeCycleHookModifier]!.push(cb);
+    return targetE;
+}
