2c2832a9dcd09b600c4d19c81da536feba485071
amir-arad <amira@wix.com>
2017-07-09 10:12:07 +0300

    remove console log

--- test/utils/config.spec.ts
+++ test/utils/config.spec.ts
@@ -77,7 +77,6 @@ describe('config', () => {
             runInContext(sampleConfig2, () => {
                 expect(getGlobalConfig(), 'inside runInContext').to.containSubset(sampleConfig2);
             });
-            console.log('exected', sampleConfig);
             expect(getGlobalConfig(), 'after runInContext').to.eql(sampleConfig);
         });
 
