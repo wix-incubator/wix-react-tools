d4817ea0f4046dec962e6d2092b0f63cad328968
Amir Arad <amira@wix.com>
2017-07-12 20:12:43 +0300

    add middleware name to warn message

--- src/utils/class-decor.ts
+++ src/utils/class-decor.ts
@@ -184,7 +184,7 @@ function runMiddlewareHooksAndOrigin<T extends object>(target: T, mixerMeta: Mix
         //Run middleware:
         retVal = middlewareHooks[0](target, createNextForMiddlewareHook(target, originalMethod, middlewareHooks, 1,reportNextMiddleware), methodArgs);
         if (lastMiddlewareRunning<middlewareHooks.length && getGlobalConfig<FlagsContext>().middlewareWarnWhenChainBreaking){
-            console.warn(`@middleware for ${target.constructor.name}.${methodName}() did not call next`);
+            console.warn(`@middleware ${middlewareHooks[lastMiddlewareRunning].name} for ${target.constructor.name}.${methodName}() did not call next`);
         }
     }else{
         // No middleware - only original function
