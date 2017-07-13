bfc216d25ef07e08e9cf7b062215eb963a0ba057
amir-arad <amira@wix.com>
2017-07-13 10:24:37 +0300

    add examples to the rest of class-decor

--- test/utils/class-decor/example.spec.ts
+++ test/utils/class-decor/example.spec.ts
@@ -1,12 +1,11 @@
 import {resetAll, spyAll} from "../../test-tools";
-import {onInstance} from "../../../src/utils/class-decor";
+import {middleware, onInstance, before, after} from "../../../src/utils/class-decor";
 import {expect} from "test-drive";
 
-let globalConsole = console;
 describe('class-decor documentation examples', () => {
     const console = spyAll({
-        log: globalConsole.log,
-        warn: globalConsole.warn,
+        log: ()=>{},
+        warn: ()=>{},
     });
 
     afterEach("reset console.warn", () => {
@@ -16,7 +15,7 @@ describe('class-decor documentation examples', () => {
     function expectLog(...lines: Array<string>) {
         expect(console.log).to.have.callCount(lines.length);
         lines.forEach((val: string, idx: number) => {
-            expect(console.log.getCall(idx).args[0], `call #${idx} argument`).eql(val);
+            expect(console.log.getCall(idx).args, `call #${idx} argument`).eql([val]);
         });
     }
 
@@ -38,13 +37,110 @@ describe('class-decor documentation examples', () => {
                     console.log('inited logger: ' + name);
                 }
             }
-            const instance = new Logger('MyLogger');
+            const logger = new Logger('MyLogger');
             expectUnorderedLog(
                 `inited logger: MyLogger`,
                 `called on constructor with "MyLogger"`
             );
-            debugger;
-            expect(instance).to.be.instanceOf(Logger);
+            expect(logger).to.be.instanceOf(Logger);
         });
     });
+
+    describe('middleware', () => {
+        it('directly on class', () => {
+            function logMW(instance: Logger, next: (n:string)=>string, methodArguments:[string]){
+                console.log('called on method with '+methodArguments[0]);
+                const result:string = next('goodbye');
+                console.log(result)
+                return 'wrapped=> '+result
+            }
+
+            @middleware(logMW, 'printMessage')
+            class Logger{
+                printMessage(text:string){
+                    console.log(text);
+                    return 'message printed: '+text;
+                }
+            }
+            const logger = new Logger();
+            const result = logger.printMessage('hello');
+            expectLog(
+                `called on method with hello`,
+                `goodbye`,
+                `message printed: goodbye`
+            );
+            expect(result).to.eql('wrapped=> message printed: goodbye');
+        });
+    });
+
+    describe('before', () => {
+        it('directly on class', () => {
+            function preMethod(instance:Logger, methodArguments:[string]){
+                console.log('called before method with '+methodArguments[0]);
+                return ['goodbye'];
+            }
+
+            @before(preMethod, 'printMessage')
+            class Logger{
+                printMessage(text:string){
+                    console.log(text);
+                    return 'message printed: '+text;
+                }
+            }
+            const logger = new Logger();
+            const result = logger.printMessage('hello');
+            expectLog(
+                `called before method with hello`,
+                `goodbye`
+            );
+            expect(result).to.eql('message printed: goodbye');
+        });
+    });
+
+    describe('after', () => {
+        it('directly on class', () => {
+            function postMethod(instance:Logger, methodReturn:string){
+                console.log(methodReturn);
+                return 'wrapped=> '+methodReturn;
+            }
+
+            @after(postMethod, 'printMessage')
+            class Logger{
+                printMessage(text:string){
+                    console.log(text);
+                    return 'message printed: '+text;
+                }
+            }
+
+            const logger = new Logger();
+            const result = logger.printMessage('hello');
+            expectLog(
+                `hello`,
+                `message printed: hello`
+            );
+            expect(result).to.eql('wrapped=> message printed: hello');
+        });
+    });
+
+/*
+    describe('add', () => {
+        it('directly on class', () => {
+            function printMessage(text:string){
+                console.log(text);
+                return 'message printed: '+text;
+            }
+
+            @add({ printMessage })
+            class Logger{
+                printMessage: (text:string)=>string;
+            }
+            const logger = new Logger();
+            const result = logger.printMessage('hello');
+            expectLog(
+                `hello`
+            );
+            expect(result).to.eql('message printed: hello');
+        });
+    });
+    */
 });
