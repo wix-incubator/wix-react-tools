07769d08b69b42866f72a194019765c662cdaf75
Amir Arad <amira@wix.com>
2017-07-16 16:32:09 +0300

    Amir/react decor (#63)
    fixes #28 
    
    * throw error if hook returns undefined
    
    * better cleanup, using ReactCurrentOwner instead of after hook
    
    * extract mixer from class-decor
    
    * support multiple hooks for create element

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from './mixins/disposable-decorator';
 export * from './mixins/global-id-decorator';
 //utils
 export * from './utils/class-decor';
+export * from './utils/react-decor';
 export * from './utils/config';
 export * from './utils/disposers';
 export * from './utils/flags';
--- test/utils/private-context.spec.ts
+++ test/utils/private-context.spec.ts
@@ -2,20 +2,20 @@ import {expect} from "test-drive";
 import {getPrivateContext,runInContext,FlagsContext} from "../../src/";
 
 let ids = ["ID0","ID1"];
-
+type State = {foo?:string};
 describe('Private context', () => {
     it('serves private context per id per instance',()=>{
         const instance = {};
-        getPrivateContext(instance,ids[0]).foo="Hi";
-        expect(getPrivateContext(instance,ids[0])).to.eql({foo:"Hi"});
-        expect(getPrivateContext(instance,ids[1])).to.eql({});  //Make sure new key generates a new object
+        getPrivateContext<State>(instance,ids[0]).foo="Hi";
+        expect(getPrivateContext<State>(instance,ids[0])).to.eql({foo:"Hi"});
+        expect(getPrivateContext<State>(instance,ids[1])).to.eql({});  //Make sure new key generates a new object
 
-        expect(getPrivateContext({},ids[0])).to.eql({});    //Check that new instance doesn't return information given to other instance
+        expect(getPrivateContext<State>({},ids[0])).to.eql({});    //Check that new instance doesn't return information given to other instance
     });
 
     it("doesn't show the added fields on original object",()=>{
         const instance = {};
-        getPrivateContext(instance,ids[0]).foo="Hi";
+        getPrivateContext<State>(instance,ids[0]).foo="Hi";
 
         expect(Object.keys(instance)).to.eql([]);
     });
@@ -23,8 +23,8 @@ describe('Private context', () => {
     it("doesn't create gazillion fields on an instance",()=>{
         runInContext<FlagsContext>({devMode:true},()=>{
             const instance = {};
-            getPrivateContext(instance,ids[0]).foo="Hi";
-            getPrivateContext(instance,ids[1]).foo="Bye";
+            getPrivateContext<State>(instance,ids[0]).foo="Hi";
+            getPrivateContext<State>(instance,ids[1]).foo="Bye";
 
             expect(Object.keys(instance).length).to.eql(1);
         });
@@ -33,7 +33,7 @@ describe('Private context', () => {
     it("doesn't let you change an instance's private context",()=>{
         runInContext<FlagsContext>({devMode:true},()=>{
             const instance = {};
-            getPrivateContext(instance,ids[0]).foo="Hi";
+            getPrivateContext<State>(instance,ids[0]).foo="Hi";
 
             const desc = Object.getOwnPropertyDescriptor(instance,Object.keys(instance)[0]);
             expect(desc).to.containSubset({writable:false,configurable:false});
