e18163ca6b0754e9cefb43012d2ee1fe7c93aea7
amir-arad <amira@wix.com>
2017-07-27 19:26:37 +0300

    clean and complete function type definition (up to 10 args) add missing typings test

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,4 +1,18 @@
-import {Increment, IsZero, ObjectHasKey, THCons, THList, THListHead, THListLength, THListReverse, THListTail, THNil} from "typelevel-ts";
+import {
+    _0,
+    _1,
+    _2, _3, _4, _5, _6, _7, _8, _9,
+    Increment,
+    Nat,
+    ObjectHasKey,
+    THCons,
+    THList,
+    THListIsTHNil,
+    THListReverse,
+    THListTail,
+    THNil,
+    UnsafeTHListGet
+} from "typelevel-ts";
 export type Class<T extends object> = new(...args: any[]) => T;
 export type GlobalConfig = {
     devMode?: boolean;
@@ -6,35 +20,26 @@ export type GlobalConfig = {
 
 export type NumberToString = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 
+export type ArgAt<A extends THList, I extends Nat> = UnsafeTHListGet<A, I>;
 
+export declare type THListLengthNum<L extends THList> = {
+    true: 0;
+    false: Increment[THListLengthNum<THListTail<L>>];
+}[THListIsTHNil<L>];
 
-export type ApiFunc<L extends THList, C, T = any> = {
-    true: (this:T) => C
-    false: {
-        true: (this:T, a: THListHead<L>) => C
-        false: {
-            true: (this:T, a: THListHead<L>, b: THListHead<THListTail<L>>) => C
-            false: {
-                true: (this:T, a: THListHead<L>, b: THListHead<THListTail<L>>, c: THListHead<THListTail<THListTail<L>>>) => C
-                false: 'error' // TODO more cases
-            }[IsZero<THListLength<THListTail<THListTail<THListTail<L>>>>>]
-        }[IsZero<THListLength<THListTail<THListTail<L>>>>]
-    }[IsZero<THListLength<THListTail<L>>>]
-}[IsZero<THListLength<L>>]
-
-export type THListToCurriedFunction<L extends THList, C> = {
-    true: () => C
-    false: {
-        true: (a: THListHead<L>) => C
-        false: {
-            true: (a: THListHead<L>) => (b: THListHead<THListTail<L>>) => C
-            false: {
-                true: (a: THListHead<L>) => (b: THListHead<THListTail<L>>) => (c: THListHead<THListTail<THListTail<L>>>) => C
-                false: 'error' // TODO more cases
-            }[IsZero<THListLength<THListTail<THListTail<THListTail<L>>>>>]
-        }[IsZero<THListLength<THListTail<THListTail<L>>>>]
-    }[IsZero<THListLength<THListTail<L>>>]
-}[IsZero<THListLength<L>>]
+export type ApiFunc<A extends THList, R = void, T = any> = {
+    0: (this: T) => R
+    1: (this: T, a0: ArgAt<A, _0>) => R
+    2: (this: T, a0: ArgAt<A, _0>, a1: ArgAt<A, _1>) => R
+    3: (this: T, a0: ArgAt<A, _0>, a1: ArgAt<A, _1>, a2: ArgAt<A, _2>) => R
+    4: (this: T, a0: ArgAt<A, _0>, a1: ArgAt<A, _1>, a2: ArgAt<A, _2>, a3: ArgAt<A, _3>) => R
+    5: (this: T, a0: ArgAt<A, _0>, a1: ArgAt<A, _1>, a2: ArgAt<A, _2>, a3: ArgAt<A, _3>, a4: ArgAt<A, _4>) => R
+    6: (this: T, a0: ArgAt<A, _0>, a1: ArgAt<A, _1>, a2: ArgAt<A, _2>, a3: ArgAt<A, _3>, a4: ArgAt<A, _4>, a5: ArgAt<A, _5>) => R
+    7: (this: T, a0: ArgAt<A, _0>, a1: ArgAt<A, _1>, a2: ArgAt<A, _2>, a3: ArgAt<A, _3>, a4: ArgAt<A, _4>, a5: ArgAt<A, _5>, a6: ArgAt<A, _6>) => R
+    8: (this: T, a0: ArgAt<A, _0>, a1: ArgAt<A, _1>, a2: ArgAt<A, _2>, a3: ArgAt<A, _3>, a4: ArgAt<A, _4>, a5: ArgAt<A, _5>, a6: ArgAt<A, _6>, a7: ArgAt<A, _7>) => R
+    9: (this: T, a0: ArgAt<A, _0>, a1: ArgAt<A, _1>, a2: ArgAt<A, _2>, a3: ArgAt<A, _3>, a4: ArgAt<A, _4>, a5: ArgAt<A, _5>, a6: ArgAt<A, _6>, a7: ArgAt<A, _7>, a8: ArgAt<A, _8>) => R
+    10: (this: T, a0: ArgAt<A, _0>, a1: ArgAt<A, _1>, a2: ArgAt<A, _2>, a3: ArgAt<A, _3>, a4: ArgAt<A, _4>, a5: ArgAt<A, _5>, a6: ArgAt<A, _6>, a7: ArgAt<A, _7>, a8: ArgAt<A, _8>, a9: ArgAt<A, _9>) => R
+}[NumberToString[THListLengthNum<A>]];
 
 export type Args<A, I = 0, L = THNil> = {
     true: Args<A, Increment[I], THCons<A[I], L>>;
--- typings-checker/index.ts
+++ typings-checker/index.ts
@@ -1,12 +1,15 @@
-import {NumberToString, ApiFunc, Args} from "../dist/src/index";
+import {ApiFunc, Args, NumberToString} from "../dist/src/index";
 
 type FIVE = NumberToString[5];
 const n2s1: FIVE = '5';
 // $ExpectError Type '"6"' is not assignable to type '"5"'
 const n2s2: FIVE = '6';
 
-declare function parseInt(string: string, radix?: number): number;
+type Ctx = { foo: number }
+type Result = { bar: number }
 
-const stringToNumber: ApiFunc<Args<[string]>, number> = parseInt;
-// $ExpectError Type 'number' is not assignable to type 'string'
-const numberToNumber: ApiFunc<Args<[number]>, number> = parseInt;
+// $ExpectType (this: Ctx, a0: "0", a1: "1", a2: "2", a3: "3", a4: "4", a5: "5", a6: "6", a7: "7", a8: "8", a9: ...
+0 as any as ApiFunc<Args<['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']>, Result, Ctx>
+
+// $ExpectType (this: Ctx, a0: "0", a1: "1", a2: "2") => Result
+0 as any as ApiFunc<Args<['0', '1', '2']>, Result, Ctx>
