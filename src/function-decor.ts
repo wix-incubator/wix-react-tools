c6d4d3b8d3cee9db02682d47484ce5428e73a9e3
amir-arad <amira@wix.com>
2017-07-27 17:06:26 +0300

    good implementation
    good type checking for middleware

--- src/function-decor.ts
+++ src/function-decor.ts
@@ -1,34 +1,30 @@
 import {ApiFunc} from "./core/types";
-import {Increment, ObjectHasKey, THCons, THList, THListReverse, THListToTuple, THNil} from "typelevel-ts";
-export type BeforeHook<A extends Array<any>, T = any> = (this: T, methodArguments: A) => A;
+import { THList, THListToTuple, THNil} from "typelevel-ts";
+export type FunctionWrapper<A extends THList, R = void, T = any> =  <F extends ApiFunc<A, R, T>>(func: F) => F
+
+export type BeforeHook<A extends THList, T = any> = (this: T, methodArguments: THListToTuple<A>) => THListToTuple<A>;
+
+export function before<A extends THList, T=any>(preMethod: BeforeHook<A, T>): FunctionWrapper<A, any, T> {
+    return function beforeWrapper<F extends ApiFunc<A, any, T>>(originalFunction: F): F {
+        return function wrapped(this:T, ...methodArguments:any[]): any {
+            return originalFunction.apply(this, preMethod.apply(this, [methodArguments]));
+        } as any as F;
+    }
+}
+
 export type AfterHook<R = void, T = any> = (this: T, methodResult: R) => R;
-export type FunctionWrapper<T extends Function> = <T1 extends T>(func: T1) => T1
 
-export function before<T extends Function>(preMethod: BeforeHook<any>): FunctionWrapper<T> {
-    return function (originalFunction: Function): any {
-        return function (...methodArguments: Array<any>): any {
-            return originalFunction(...preMethod(methodArguments));
-        }
+export function after<R=void, T=any>(postMethod: AfterHook<R, T>): FunctionWrapper<any, R, T> {
+    return function afterWrapper<F extends ApiFunc<any, R, T>>(originalFunction: F): F {
+        return function wrapped(this:T, ...methodArguments:any[]): R {
+            return postMethod.call(this, originalFunction.apply(this, methodArguments));
+        } as any as F;
     }
 }
-
-export function after<T extends Function>(postMethod: AfterHook<any>): FunctionWrapper<T> {
-    return function (originalFunction: Function): any {
-        return function (methodResult: any) {
-            return postMethod(originalFunction(methodResult));
-        }
-    }
-}
-
-export type _FunctionWrapper<A, R = void, T = any, I = 0, L = THNil> =  {
-    true: _FunctionWrapper<A, R, T, Increment[I], THCons<A[I], L>>;
-    false: (func: ApiFunc<THListReverse<L>, R, T>) => ApiFunc<THListReverse<L>, R, T>
-}[ObjectHasKey<A, I>];
-
 
 export type MiddlewareHook<A extends THList, R = void, T = any> = (this: T, next: (methodArguments: THListToTuple<A>) => R, methodArguments: THListToTuple<A>) => R;
 
-export function middleware<A extends THList, R=void, T=any>(hook: MiddlewareHook<A, R, T>) {
+export function middleware<A extends THList, R=void, T=any>(hook: MiddlewareHook<A, R, T>): FunctionWrapper<A, R, T> {
     return function middlewareWrapper<F extends ApiFunc<A, R, T>>(originalFunction: F): F {
         return function wrapped(this:T, ...methodArguments: any[]): R {
             function next(this:T, args: THListToTuple<A>){
--- typings-checker/function-decor.ts
+++ typings-checker/function-decor.ts
@@ -1,21 +1,43 @@
-import {middleware, _FunctionWrapper, Args} from "../dist/src/index";
+import {Args, middleware} from "../dist/src/index";
 
 declare function original(text: string): string;
+declare function specific(text: 'foo'): 'bar';
+declare function moreSpecific(this:'fooBar', text: 'foo'): 'bar';
 declare function fake(text: number): string;
 
-declare function mwHook(next: (methodArguments: [string]) => string, methodArguments: [string]):string;
-declare function fakeMwHook(next: (methodArguments: [number]) => string, methodArguments: [number]):string;
+declare function mwHook(next: (methodArguments: [string]) => string, methodArguments: [string]): string;
+declare function specificMwHook(next: (methodArguments: ['foo']) => 'bar', methodArguments: ['foo']): 'bar';
+declare function fakeMwHook(next: (methodArguments: [number]) => string, methodArguments: [number]): string;
 
-const e1: typeof original = middleware(mwHook)(original);
-// $ExpectError Type 'string' is not assignable to type 'number'
-const e2: typeof original  = middleware(mwHook)(fake);
-const e3: typeof original  = middleware<Args<[string]>, string>(mwHook)(original);
-// $ExpectError Type 'string' is not assignable to type 'number'
-middleware<Args<[string]>, string>(mwHook)(fake);
+describe(`Middlewares`, () => {
+    describe(`Test original type`, () => {
+        // $ExpectType (text: string) => string
+        middleware<Args<[string]>, string>(mwHook)(original);
 
-const e5: typeof original  = middleware(fakeMwHook)(original);
-// $ExpectError Type '(text: number) => string' is not assignable to type '(text: string) => string'.
-const e6: typeof original  = middleware(fakeMwHook)(fake);
-// $ExpectError Type 'number' is not assignable to type 'string'.
-middleware<Args<[string]>, string>(fakeMwHook);
+        // $ExpectType (text: string) => string
+        middleware(mwHook)(original);
+    })
+    describe(`Test with an incompatible type ('fake')`, () => {
+        // $ExpectType (text: number) => string
+        middleware(mwHook)(fake);
 
+        // $ExpectType (text: string) => string
+        middleware(fakeMwHook)(original);
+
+        // $ExpectError Type 'string' is not assignable to type 'number'
+        middleware<Args<[string]>, string>(mwHook)(fake);
+
+        // $ExpectError Type 'number' is not assignable to type 'string'.
+        middleware<Args<[string]>, string>(fakeMwHook);
+    })
+    describe(`Test with a subset ('specific') `, () => {
+        // $ExpectType (text: "foo") => "bar"
+        middleware(mwHook)(specific);
+
+        // $ExpectType (text: "foo") => "bar"
+        middleware<Args<[string]>, string>(mwHook)(specific);
+
+        // $ExpectError Type 'string' is not assignable
+        middleware<Args<['foo']>, 'bar'>(mwHook);
+    })
+})
