3b9e63599335c6ae2709b1205c7fd2b519e7d5f7
amir-arad <amira@wix.com>
2017-07-27 17:26:13 +0300

    even better middleware types test

--- typings-checker/function-decor.ts
+++ typings-checker/function-decor.ts
@@ -1,43 +1,53 @@
 import {Args, middleware} from "../dist/src/index";
 
 declare function original(text: string): string;
-declare function specific(text: 'foo'): 'bar';
-declare function moreSpecific(this:'fooBar', text: 'foo'): 'bar';
-declare function fake(text: number): string;
+declare function subset(text: 'foo'): 'bar';
+declare function incompatible(text: number): string;
 
 declare function mwHook(next: (methodArguments: [string]) => string, methodArguments: [string]): string;
-declare function specificMwHook(next: (methodArguments: ['foo']) => 'bar', methodArguments: ['foo']): 'bar';
-declare function fakeMwHook(next: (methodArguments: [number]) => string, methodArguments: [number]): string;
+declare function subsetMwHook(next: (methodArguments: ['foo']) => 'bar', methodArguments: ['foo']): 'bar';
+declare function incompatibleMwHook(next: (methodArguments: [number]) => string, methodArguments: [number]): string;
 
 describe(`Middlewares`, () => {
     describe(`Test original type`, () => {
+
         // $ExpectType (text: string) => string
         middleware<Args<[string]>, string>(mwHook)(original);
 
         // $ExpectType (text: string) => string
         middleware(mwHook)(original);
     })
-    describe(`Test with an incompatible type ('fake')`, () => {
-        // $ExpectType (text: number) => string
-        middleware(mwHook)(fake);
+    describe(`Test with an incompatible type`, () => {
+        describe(`documentation only (TODO: make it break)`, () => {
 
-        // $ExpectType (text: string) => string
-        middleware(fakeMwHook)(original);
+            // $ExpectType (text: number) => string
+            middleware(mwHook)(incompatible);
+
+            // $ExpectType (text: string) => string
+            middleware(incompatibleMwHook)(original);
+        })
 
         // $ExpectError Type 'string' is not assignable to type 'number'
-        middleware<Args<[string]>, string>(mwHook)(fake);
+        middleware<Args<[string]>, string>(mwHook)(incompatible);
 
         // $ExpectError Type 'number' is not assignable to type 'string'.
-        middleware<Args<[string]>, string>(fakeMwHook);
+        middleware<Args<[string]>, string>(incompatibleMwHook);
     })
-    describe(`Test with a subset ('specific') `, () => {
-        // $ExpectType (text: "foo") => "bar"
-        middleware(mwHook)(specific);
+    describe(`Test with a subset`, () => {
 
         // $ExpectType (text: "foo") => "bar"
-        middleware<Args<[string]>, string>(mwHook)(specific);
+        middleware(mwHook)(subset);
+
+        // $ExpectType (text: "foo") => "bar"
+        middleware<Args<[string]>, string>(mwHook)(subset);
 
         // $ExpectError Type 'string' is not assignable
         middleware<Args<['foo']>, 'bar'>(mwHook);
+
+        describe(`documentation only (TODO: make it break)`, () => {
+
+            // $ExpectType (text: string) => string
+            middleware<Args<[string]>, string>(subsetMwHook)(original)
+        })
     })
 })
