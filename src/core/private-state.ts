55b5f590f6cca80db75221c0548303bfed39cb0e
amir-arad <amira@wix.com>
2017-08-02 18:37:30 +0300

    add `.hasState` to private state

--- src/core/private-state.ts
+++ src/core/private-state.ts
@@ -3,42 +3,68 @@ import {getGlobalConfig} from "./config";
 export const STATE_DEV_MODE_KEY = "$private-context";
 const privates = new WeakMap();
 
-/**
- * a function that provides a private context per instance.
- * @param targetObj object to which the private context is affiliated.
- */
-export type StateProvider<P extends object = any, T extends object = any> = (targetObj: T) => P
+
+export interface StateProvider<P extends object = any, T extends object = any>{
+    /**
+     * a function that provides a private state per instance. initializes a new state if none exists.
+     * @param targetObj object to which the private state is affiliated.
+     */
+    (targetObj: T): P;
+    /**
+     * checks whether an instance has a private state associated with it. does not initialize a new state.
+     * @param targetObj object to which a private state may be affiliated.
+     */
+    hasState(targetObj: T): boolean;
+}
 
 /**
- * Returns a function that provides a private context per instance.
+ * Returns a function that provides a private state per instance.
  * @param key a unique identifier for the provider
  * @param initializer {StateProvider<P, T>} initializer of a new private state.
  * @returns {StateProvider<P, T>} Provider for the private state object.
  */
-export function privateState<P extends object = any, T extends object = any>(key: string, initializer: StateProvider<P, T>): StateProvider<P, T> {
-    return function (targetObj: T) {
-        let privateContext: { [key: string]: P };
-        if (getGlobalConfig().devMode) {
-            const targetObject = targetObj as any;
-            if (targetObject.hasOwnProperty(STATE_DEV_MODE_KEY)) {
-                privateContext = targetObject[STATE_DEV_MODE_KEY];
-            } else {
-                // create a new private context
-                Object.defineProperty(targetObject, STATE_DEV_MODE_KEY, {value: privateContext = {}});
-            }
-        } else {
-            if (privates.has(targetObj)) {
-                privateContext = privates.get(targetObj);
-            } else {
-                privates.set(targetObj, privateContext = {});
-            }
-        }
-
+export function privateState<P extends object = any, T extends object = any>(key: string, initializer: {(targetObj: T): P}): StateProvider<P, T> {
+    const result = function getPrivateState(targetObj: T) {
+        let privateContext: { [key: string]: P } = getStateContext(targetObj, true);
         // If key doesn't exist for that instance, create a new object for that key
         if (!privateContext[key]) {
             // init instance-key
             privateContext[key] = initializer(targetObj);
         }
         return privateContext[key];
+    } as StateProvider<P, T>;
+
+    result.hasState = function hasState(targetObj: T):boolean {
+        let stateContext = getStateContext(targetObj, false);
+        return null !== stateContext && undefined !== stateContext[key];
+    };
+    return result
+}
+
+/**
+ * provides a store for private states, affiliated with the instance
+ * @param targetObj object to which a store may be affiliated.
+ * @param initIfNone initialize a new store if none is affilitated with targetObj
+ * @returns a context, or null if none existed and initIfNone was set to false
+ */
+function getStateContext<T extends object = any>(targetObj: T, initIfNone:true):{ [key: string]: any };
+function getStateContext<T extends object = any>(targetObj: T, initIfNone:false):{ [key: string]: any } | null;
+function getStateContext<T extends object = any>(targetObj: T, initIfNone:boolean):{ [key: string]: any } | null {
+    let privateContext: { [key: string]: any } | null = null;
+    if (getGlobalConfig().devMode) {
+        const targetObject = targetObj as any;
+        if (targetObject.hasOwnProperty(STATE_DEV_MODE_KEY)) {
+            privateContext = targetObject[STATE_DEV_MODE_KEY];
+        } else if (initIfNone){
+            // create a new private context
+            Object.defineProperty(targetObject, STATE_DEV_MODE_KEY, {value: privateContext = {}});
+        }
+    } else {
+        if (privates.has(targetObj)) {
+            privateContext = privates.get(targetObj);
+        } else if (initIfNone){
+            privates.set(targetObj, privateContext = {});
+        }
     }
+    return privateContext;
 }
--- test/core/private-context.spec.ts
+++ test/core/private-context.spec.ts
@@ -10,65 +10,90 @@ type State = {
     bar?: string
 };
 describe('Private state', () => {
-    describe('per ID', () => {
-        const pState0 = privateState<State>(ids[0], emptyState);
-        const pState1 = privateState<State>(ids[1], emptyState);
-        const pState2 = privateState<State>(ids[2], (subj: any) => {
-            const result = Object.create(pState1(subj));
-            result.foo = 'bar';
-            return result;
-        });
-        it('serves private state per id per instance', () => {
+    const pState0 = privateState<State>(ids[0], emptyState);
+    const pState1 = privateState<State>(ids[1], emptyState);
+    const pState2 = privateState<State>(ids[2], (subj: any) => {
+        const result = Object.create(pState1(subj));
+        result.foo = 'bar';
+        return result;
+    });
+    it('serves private state per id per instance', () => {
+        const instance = {};
+        pState0(instance).foo = "Hi";
+        expect(pState0(instance)).to.eql({foo: "Hi"});
+        expect(pState1(instance)).to.eql({});  //Make sure new key generates a new object
+
+        expect(pState0({})).to.eql({});    //Check that new instance doesn't return information given to other instance
+    });
+
+    it("doesn't show the added fields on original object", () => {
+        const instance = {};
+        pState0(instance).foo = "Hi";
+
+        expect(Object.keys(instance)).to.eql([]);
+    });
+
+    it("doesn't create gazillion fields on an instance", () => {
+        runInContext({devMode: true}, () => {
             const instance = {};
             pState0(instance).foo = "Hi";
-            expect(pState0(instance)).to.eql({foo: "Hi"});
-            expect(pState1(instance)).to.eql({});  //Make sure new key generates a new object
+            pState1(instance).foo = "Bye";
 
-            expect(pState0({})).to.eql({});    //Check that new instance doesn't return information given to other instance
+            expect(Object.keys(instance).length).to.be.lessThan(2);
         });
+    });
 
-        it("doesn't show the added fields on original object", () => {
+    it("in dev mode, expose an instance's private state but doesn't let you change it", () => {
+        runInContext({devMode: true}, () => {
             const instance = {};
             pState0(instance).foo = "Hi";
 
-            expect(Object.keys(instance)).to.eql([]);
+            const desc = Object.getOwnPropertyDescriptor(instance, STATE_DEV_MODE_KEY);
+            expect(desc).to.containSubset({writable: false, configurable: false});
         });
+    });
 
-        it("doesn't create gazillion fields on an instance", () => {
-            runInContext({devMode: true}, () => {
-                const instance = {};
-                pState0(instance).foo = "Hi";
-                pState1(instance).foo = "Bye";
+    it("outside dev mode, do not expose an instance's private state ", () => {
+        runInContext({devMode: false}, () => {
+            const instance = {};
+            pState0(instance).foo = "Hi";
 
-                expect(Object.keys(instance).length).to.be.lessThan(2);
-            });
+            expect(instance.hasOwnProperty(STATE_DEV_MODE_KEY)).to.eql(false);
+            expect((instance as any)[STATE_DEV_MODE_KEY]).to.equal(undefined);
         });
+    });
 
-        it("in dev mode, expose an instance's private state but doesn't let you change it", () => {
-            runInContext({devMode: true}, () => {
-                const instance = {};
-                pState0(instance).foo = "Hi";
+    it("allows initializing (and prototype inheritance) between states", () => {
+        const instance = {};
+        expect(pState2(instance)).to.eql({foo: "bar"});
+        pState1(instance).bar = "Hi";
+        expect(pState2(instance)).to.eql({foo: "bar", bar: "Hi"});
+    });
 
-                const desc = Object.getOwnPropertyDescriptor(instance, STATE_DEV_MODE_KEY);
-                expect(desc).to.containSubset({writable: false, configurable: false});
-            });
-        });
-
-        it("outside dev mode, do not expose an instance's private state ", () => {
+    describe('.hasState', () => {
+        it('does not change the original instance', () => {
             runInContext({devMode: false}, () => {
                 const instance = {};
-                pState0(instance).foo = "Hi";
-
-                expect(instance.hasOwnProperty(STATE_DEV_MODE_KEY)).to.eql(false);
-                expect((instance as any)[STATE_DEV_MODE_KEY]).to.equal(undefined);
+                pState0.hasState(instance);
+                expect(instance).to.eql({});
             });
         });
-
-        it("allows initializing (and prototype inheritance) between states", () => {
+        it('in dev mode, does not change the original instance', () => {
             const instance = {};
-            expect(pState2(instance)).to.eql({foo:"bar"});
-            pState1(instance).bar = "Hi";
-            expect(pState2(instance)).to.eql({foo:"bar", bar: "Hi"});
+            pState0.hasState(instance);
+            expect(instance).to.eql({});
+            runInContext({devMode: true}, () => {
+                const instance = {};
+                pState0.hasState(instance);
+                expect(instance).to.eql({});
+            });
+        });
+        it('reflects existence of private state per id per instance', () => {
+            const instance = {};
+            expect(pState0.hasState(instance)).to.eql(false);
+            pState0(instance);
+            expect(pState0.hasState(instance)).to.eql(true);
+            expect(pState1.hasState(instance)).to.eql(false);
         });
     });
 });
