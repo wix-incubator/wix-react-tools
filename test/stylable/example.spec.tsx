3b75cbdd26643a3d7e16800879d33f1859e196cc
amir-arad <amira@wix.com>
2017-08-10 17:28:49 +0300

    add regression to stylable react decorator
    Signed-off-by: amir-arad <amira@wix.com>

---
+++ test/stylable/example.spec.tsx
@@ -0,0 +1,65 @@
+import {SBComponent} from "stylable-react-component/dist/stylable-react";
+import {fromCSS} from "stylable";
+import {ClientRenderer, expect} from "test-drive-react";
+import * as React from "react";
+
+
+describe('stylable', () => {
+
+    const clientRenderer = new ClientRenderer();
+    afterEach(() => clientRenderer.cleanup());
+
+    it('supports class names', () => {
+
+        const css = fromCSS(`
+    .classA {
+    
+    }
+`);
+        @SBComponent(css)
+        class Comp extends React.Component {
+            render() {
+                return <div data-automation-id="1">
+                    <div data-automation-id="2" className="classA fooBar"></div>
+                </div>
+            }
+        }
+        const {select, container} = clientRenderer.render(<Comp></Comp>);
+        expect(select('1')).to.equal(container.querySelector(`.${css.classes.root}`));
+        expect(container.querySelectorAll(`.${css.classes.root}`)).to.have.length(1);
+        expect(select('2')).to.equal(container.querySelector(`.${css.classes.classA}`));
+        expect(select('2')).to.equal(container.querySelector(`.fooBar`));
+        expect(container.querySelectorAll(`.${css.classes.classA}`)).to.have.length(1);
+    });
+
+    it('supports style state', () => {
+        const css = fromCSS(`
+    .root {
+        -st-state:a,b;
+    }
+    .classA {
+        -st-state:x,y;
+    }
+`);
+        const state = {x:true, y:false};
+        const rootState = {a:true, b:false};
+        const stateAttrName = Object.keys(css.cssStates(state))[0];
+        const rootStateAttrName = Object.keys(css.cssStates(rootState))[0];
+
+        @SBComponent(css)
+        class Comp extends React.Component {
+            render() {
+                return <div data-automation-id="1" cssStates={{a:true, b:false}}>
+                    <div data-automation-id="2" className="classA" cssStates={state}></div>
+                </div>
+            }
+        }
+        const {select} = clientRenderer.render(<Comp ></Comp>);
+
+        expect(select('1')!.attributes.getNamedItem(rootStateAttrName)).to.be.ok;
+        expect(select('2')!.attributes.getNamedItem(stateAttrName)).to.be.ok;
+        expect(new Comp().render().props).to.not.haveOwnProperty('cssState');
+        expect(new Comp().render().props.children.props).to.not.haveOwnProperty('cssState');
+
+    });
+});
