fca76f2efcbb7a2fbdf9c3834ed2e80d62200ce0
tomr <tomr@wix.com>
2017-08-19 20:47:32 +0300

    refactored decor react function to use decorFunction instead of middleware. Added/refactored tests

--- src/react/react-decor-function.ts
+++ src/react/react-decor-function.ts
@@ -1,21 +1,14 @@
 import React = require('react');
 import {
     Attributes,
+    ReactElement,
     ReactNode,
-    ReactHTML,
-    ReactSVG,
     SFC,
-    ComponentClass,
-    ClassType,
-    ClassicComponent,
-    cloneElement,
-    ComponentState,
-    ClassicComponentClass
+    cloneElement
 } from 'react';
-import { after, before, FunctionWrapper, middleware, MiddlewareHook } from '../function-decor';
+import { BeforeHook, AfterHook, decorFunction, HookWrappers } from '../function-decor';
 import { THList, THListToTuple } from "typelevel-ts";
 import { cloneDeep } from 'lodash';
-
 
 export type ElementHook = <P = object>(componentProps: any, args: ElementArgs<P>) => ElementArgs<P>;
 export type RootNodeHook = <P = object>(componentProps: any, rootNodeProps: Attributes & Partial<P>) => Attributes & Partial<P>;
@@ -28,7 +21,6 @@ export type ElementArgs<P extends {}> = {
 export type CreateElementArgsTuple<P extends {}> = [any, Attributes & Partial<P>, ReactNode];
 
 type ReactCreateElement = typeof React.createElement;
-
 const originalCreateElement = React.createElement;
 
 export interface DecorReactHooks {
@@ -36,65 +28,86 @@ export interface DecorReactHooks {
     root?: Array<ElementHook>
 }
 
-export function decorReact(hooks: DecorReactHooks): Function {
-    const mw = (next: Function, args: [object]) => {
-        let renderResult, createArgsMap: Map<object, ElementArgs<any>> = new Map();
+function hooksDefined(hooks: Array<ElementHook> | undefined): boolean {
+    return !!(hooks && (hooks.length > 0))
+}
 
-        try {
-            const componentProps = args[0]; // [0] for props in a functional react component
-            let wrappedCreateElement = makeCustomCreateElement(hooks, componentProps, createArgsMap);
-            React.createElement = wrappedCreateElement;
+function getHooksReducer(componentProps: object): (res: ElementArgs<any>, hook: ElementHook) => ElementArgs<any> {
+    return <P extends {}>(res: ElementArgs<P>, hook: ElementHook) => hook(componentProps, res);
+};
 
-            renderResult = next(args);
+export function decorReact<T = any>(hooks: DecorReactHooks): (comp: SFC<T>) => SFC<T> {
+    let componentProps: Attributes & Partial<any>;
+    let createArgsMap: Map<object, ElementArgs<any>> = new Map();
+    const decors: {before: Array<BeforeHook<any>>, after: Array<AfterHook<any>>} = {
+        before: [],
+        after: []
+    };
 
-            if (hooks.root && hooks.root.length > 0) {
+    if (hooksDefined(hooks.nodes) || hooksDefined(hooks.root)) {
+        const replaceCreateElement = (args: CreateElementArgsTuple<any>): CreateElementArgsTuple<any> => {
+            componentProps = args[0]; // [0] for props in a functional react component
+            React.createElement = makeCustomCreateElement(hooks, componentProps, createArgsMap);
+            return (args as CreateElementArgsTuple<any>);
+        };
+
+        const applyRootHooks = <P extends {}>(renderResult: ReactElement<P>): ReactElement<P> => {
+            if (hooksDefined(hooks.root)) {
                 let rootElementArgs = createArgsMap.get(renderResult);
 
-                rootElementArgs = hooks.root.reduce(<P extends {}>(elementArgs: ElementArgs<P>, hook: ElementHook): ElementArgs<P> => hook(componentProps, elementArgs), rootElementArgs);
+                rootElementArgs = (hooks as { root: Array<ElementHook> }).root.reduce(getHooksReducer(componentProps), rootElementArgs);
 
                 if (rootElementArgs) {
                     renderResult = cloneElement(renderResult, rootElementArgs.elementProps);
+                } else {
+                    console.warn('unable to found matching component for: ', rootElementArgs); // todo: test this?
                 }
             }
-        } finally {
-            React.createElement = originalCreateElement as ReactCreateElement;
-            createArgsMap.clear();
-        }
 
-        return renderResult;
-    }
-    return middleware(mw);
+            createArgsMap.clear();
+            return renderResult;
+        };
+
+        decors.before.push(replaceCreateElement);
+        decors.after.push(applyRootHooks);
+        React.createElement = originalCreateElement as ReactCreateElement;
+    };
+
+    return decorFunction(decors) as (comp: SFC<T>) => SFC<T> ; // as SFC?
 }
 
 // create a custom react create element function that applies the given hooks
-function makeCustomCreateElement<P extends {}>(hooks: DecorReactHooks, componentProps: object, createArgsMap: Map<object, ElementArgs<P>>): typeof originalCreateElement {
-    let wrappedCreateElement = originalCreateElement;
+function makeCustomCreateElement<P extends {}>(hooks: DecorReactHooks, componentProps: object, createArgsMap: Map<object, ElementArgs<P>>): typeof React.createElement {
+    if (hooksDefined(hooks.nodes) || hooksDefined(hooks.root)) {
+        let createElementArgsObject: ElementArgs<P>;
 
-    const executeHook = (res: ElementArgs<any>, hook: Function): ElementArgs<any> => {
-        return hook(componentProps, res);
-    };
-
-    if ((hooks.nodes && hooks.nodes.length) || (hooks.root && hooks.root.length)) {
-        const mwHook = (next: Function, createElementArgsTuple: CreateElementArgsTuple<any>) => {
-            let createElementArgsObject = translateArgumentsToObject(createElementArgsTuple);
-            if (hooks.nodes && hooks.nodes.length) {
-                createElementArgsObject = hooks.nodes.reduce(executeHook, createElementArgsObject);
-                createElementArgsTuple = translateObjectToArguments(createElementArgsObject);
+        const applyHooksOnArguments = (createElementArgsTuple: CreateElementArgsTuple<P>): CreateElementArgsTuple<P> => {
+            createElementArgsObject = translateArgumentsToObject(createElementArgsTuple);
+            if (hooksDefined(hooks.nodes)) {
+                createElementArgsObject = (hooks as { nodes: Array<ElementHook> }).nodes.reduce(getHooksReducer(componentProps), createElementArgsObject);
+                return createElementArgsTuple = translateObjectToArguments(createElementArgsObject);
             }
 
-            let createElementResult = next(createElementArgsTuple);
-            createArgsMap.set(createElementResult, createElementArgsObject);
+            return createElementArgsTuple;
+        };
 
+        const saveCreateElementArguments = (createElementResult: ReactNode): ReactNode => {
+            if (createElementResult) {
+                createArgsMap.set((createElementResult as any), createElementArgsObject); // TODO: react create element can return many things, what do?
+            }
             return createElementResult;
         };
 
-        wrappedCreateElement = middleware(mwHook)(originalCreateElement);
+        return decorFunction({
+            before: [applyHooksOnArguments],
+            after: [saveCreateElementArguments]
+        })(originalCreateElement);
+    } else {
+        return originalCreateElement;
     }
-
-    return wrappedCreateElement;
 }
 
-export function translateArgumentsToObject<P extends object>(args: CreateElementArgsTuple<P>): ElementArgs<P> {
+export function translateArgumentsToObject<P extends {}>(args: CreateElementArgsTuple<P>): ElementArgs<P> {
     return {
         type: args[0],
         elementProps: args[1],
@@ -102,6 +115,6 @@ export function translateArgumentsToObject<P extends object>(args: CreateElement
     };
 }
 
-export function translateObjectToArguments<P extends object>(args: ElementArgs<P>): CreateElementArgsTuple<P> {
+export function translateObjectToArguments<P extends {}>(args: ElementArgs<P>): CreateElementArgsTuple<P> {
     return [args.type, args.elementProps, ...args.children] as CreateElementArgsTuple<P>;
 }
--- test/react/react-decor-function.spec.tsx
+++ test/react/react-decor-function.spec.tsx
@@ -4,7 +4,7 @@ import { spyAll, resetAll } from '../test-drivers/test-tools';
 import { inBrowser } from "mocha-plugin-env/dist/src";
 import { decorReact, ElementHook, ElementArgs, CreateElementArgsTuple, translateArgumentsToObject, translateObjectToArguments } from '../../src/react/react-decor-function';
 
-describe.assuming(inBrowser(), 'only in browser')('react-decor', () => {
+describe.assuming(inBrowser(), 'only in browser')('react-decor-function', () => {
     const clientRenderer = new ClientRenderer();
 
     const console = spyAll({
@@ -12,21 +12,24 @@ describe.assuming(inBrowser(), 'only in browser')('react-decor', () => {
         }
     });
 
-    afterEach("reset console.warn", () => {
+    afterEach("cleanup and reset console.log", () => {
         resetAll(console);
-    });
-
-    afterEach(() => {
         clientRenderer.cleanup();
     });
 
-    const Comp: React.SFC<{ name: string }> = ({ name }) => (<div data-delete-me="TBDeleted" data-change-me="TBChanged">
-        <span data-automation-id="content">
-            {name}
-        </span>
-    </div>);
+    type PropsWithName = {
+        name: string
+    };
 
-    const nodeHook: ElementHook = function (componentProps: { name: string }, args: ElementArgs<any>): ElementArgs<any> {
+    const Comp: React.SFC<PropsWithName> = ({ name }) => (
+        <div data-delete-me="TBDeleted" data-change-me="TBChanged">
+            <span data-automation-id="content">
+                {name}
+            </span>
+        </div>
+    );
+
+    const nodeHook: ElementHook = function (componentProps: PropsWithName, args: ElementArgs<any>): ElementArgs<any> {
         console.log(args.elementProps['data-automation-id']);
         return args;
     };
@@ -50,6 +53,9 @@ describe.assuming(inBrowser(), 'only in browser')('react-decor', () => {
             clientRenderer.render(<WrappedComp name="Jon" />);
 
             expect(console.log).to.have.callCount(2);
+            expect(console.log).to.have.been.calledWith(undefined); // no data-automation-id for root
+            expect(console.log).to.have.been.calledWithMatch(/content/);
+
         });
 
         it('should allow adding multiple nodes hooks to a stateless react component', () => {
@@ -59,18 +65,45 @@ describe.assuming(inBrowser(), 'only in browser')('react-decor', () => {
             clientRenderer.render(<WrappedComp name="Jon" />);
 
             expect(console.log).to.have.callCount(4);
+            expect(console.log.getCall(0)).to.have.been.calledWithMatch(/content/);
+            expect(console.log.getCall(1)).to.have.been.calledWithMatch(/content/);
+            expect(console.log.getCall(2)).to.have.been.calledWith(undefined); // second hook, no root id
+            expect(console.log.getCall(3)).to.have.been.calledWith(undefined); // first hook, no root id
+        });
+
+        it('should allow adding a node hook to a stateless component that will add/remove/change the element props', () => {
+            function multiActionNodeHook(componentProps: PropsWithName, args: ElementArgs<any>): ElementArgs<any> {
+                args.elementProps['data-automation-id'] = index;
+                args.elementProps['data-change-me'] = componentProps.name + index;
+                args.elementProps['data-delete-me'] = undefined;
+                index++;
+                return args;
+            }
+            let index = 0;
+
+            const wrap = decorReact({ nodes: [multiActionNodeHook] });
+            const WrappedComp = wrap(Comp);
+
+            const { select } = clientRenderer.render(<WrappedComp name="Jon" />);
+
+            expect(select('0')).to.be.ok; // add attribute
+            expect(select('1')).to.be.ok;
+            expect(select('0')).to.not.have.attribute('data-delete-me'); // remove attribute
+            expect(select('1')).to.not.have.attribute('data-delete-me');
+            expect(select('0')).to.have.attribute('data-change-me', 'Jon0'); // change/add attribute
+            expect(select('1')).to.have.attribute('data-change-me', 'Jon1');
         });
     });
 
     describe('root hooks', () => {
-        function rootHook(componentProps: { name: string }, args: ElementArgs<any>): ElementArgs<any> {
+        function rootHook(componentProps: PropsWithName, args: ElementArgs<any>): ElementArgs<any> {
             args.elementProps['data-automation-id'] = 'root';
             args.elementProps['data-change-me'] = componentProps.name;
             args.elementProps['data-delete-me'] = undefined;
             return args;
         }
 
-        it('should allow adding a single root hook to a stateless component', () => {
+        it('should allow adding a single root hook to a stateless component that will add/remove/change the root elements props', () => {
             const wrap = decorReact({ root: [rootHook] });
             const WrappedComp = wrap(Comp);
 
