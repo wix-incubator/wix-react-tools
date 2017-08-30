af5f0f90d86b932298c4855eaba45b7578f487bc
amir-arad <amira@wix.com>
2017-08-30 13:15:06 +0300

    fix small rejects rename decorators to features

--- src/react-features/stylable-feature.ts
+++ src/react-features/stylable-feature.ts
@@ -1,31 +1,31 @@
-import {RuntimeStylesheet, StateMap, Stylesheet} from "stylable";
-import {__rest} from "tslib";
+import {RuntimeStylesheet, Stylesheet} from "stylable";
 import {decorateReactComponent} from "../react-decor/index";
 import {ElementArgs, StatelessElementHook} from "../react-decor/common";
 
-const STATE_ATTR_ARR = ['style-state'];
-
-function eachElementHook(sheet: Stylesheet) : StatelessElementHook<any>{
+function eachElementHook(sheet: Stylesheet): StatelessElementHook<any> {
     function classNameMapper(name: string) {
         return sheet.get(name) || name;
     }
 
-    return function stylableEachElementHook(_instance: never, _props:any, args: ElementArgs<any>): ElementArgs<any> {
+    return function stylableEachElementHook(_instance: never, _props: any, args: ElementArgs<any>): ElementArgs<any> {
         if (typeof args.elementProps.className === 'string') {
             args.elementProps.className = args.elementProps.className.split(' ').map(classNameMapper).join(' ');
         }
         const cssStates = args.elementProps['style-state'];
         if (cssStates) {
-            const otherProps = __rest(args.elementProps, STATE_ATTR_ARR);
-            args.elementProps = {...sheet.cssStates(cssStates), ...otherProps};
+            args.elementProps = {...args.elementProps, ...sheet.cssStates(cssStates)};
         }
         return args;
     }
 }
 
 function rootElementHook(sheet: Stylesheet) {
-    return function stylableRootElementHook(_instance: never, _props:any, args: ElementArgs<any>): ElementArgs<any> {
-        args.elementProps.className = sheet.get(sheet.root) + ' ' + args.elementProps.className;
+    return function stylableRootElementHook(_instance: never, _props: any, args: ElementArgs<any>): ElementArgs<any> {
+        if (args.elementProps.className) {
+            args.elementProps.className = sheet.get(sheet.root) + ' ' + args.elementProps.className;
+        } else {
+            args.elementProps.className = sheet.get(sheet.root);
+        }
         return args;
     }
 }
