b8193515b1e9dc2829f7e94aed853a621423aef6
Lior Zisman <liorzi@wix.com>
2017-08-30 11:24:27 +0300

    added karma-sauce-launcher

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const testsSetup = [path.join(__dirname, 'dist', 'test', 'setup.js')];
 module.exports = {
     devtool: 'eval',
     entry: {
-        test: testsSetup.concat(testFiles),
+        test: ['core-js/shim', ...testsSetup.concat(testFiles)],
         webtest: ['core-js/shim', ...testsSetup.concat(testFiles.map(fileName => `mocha-loader!${fileName}`))]
     },
     output: {
