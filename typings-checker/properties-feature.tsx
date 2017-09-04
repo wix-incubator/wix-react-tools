c81d026202eb027e466e80c51a3930a5a6ebdc25
Avi Vahl <avi.vahl@wix.com>
2017-09-04 16:34:28 +0300

    Document the properties decorator (#137)
    * Document the properties decorator
    Also added a Getting Started section
    
    * rewrite documentation addition
    renamed folders accordingly

--- typings-checker/properties-feature.tsx
+++ typings-checker/properties-feature.tsx
@@ -1,29 +1,41 @@
-import { properties } from "../dist/src/index";
+import {properties} from "../dist/src/index";
 import * as React from "react";
-import {ComponentProps} from "../src/react-features/root-props";
 
 interface Props {
-    name:string;
+    name: string;
 }
 
 type VerboseProps = properties.Props & Props;
 
-@properties
-class Comp2 extends React.Component<VerboseProps> {
-    render() {
-        return <div data-automation-id="Root"/>;
-    }
-}
-properties((p:VerboseProps)=><div data-automation-id="Root"/>)
+describe(`with porperties type that includes properties.Props`, () => {
+    it(`can decorate class components`, () => {
+        @properties
+        class Comp extends React.Component<VerboseProps> {
+            render() {
+                return <div data-automation-id="Root"/>;
+            }
+        }
+    });
 
-// $ExpectError 'typeof Comp' is not assignable to
-@properties
-class Comp extends React.Component<Props> {
-    render() {
-        return <div data-automation-id="Root"/>;
-    }
-}
+    it(`can decorate functional components`, () => {
+        properties((p: VerboseProps) => <div data-automation-id="Root"/>)
+    });
+});
 
+describe(`with porperties type that does not include properties.Props`, () => {
+    it(`error decorating class components`, () => {
+// $ExpectError 'typeof Comp2' is not assignable to
+        @properties
+        class Comp2 extends React.Component<Props> {
+            render() {
+                return <div data-automation-id="Root"/>;
+            }
+        }
+
+    });
+
+    it(`error wrapping SFC functional components`, () => {
 // $ExpectError '(p: Props) => Element' is not assignable to
-properties((p:Props)=><div data-automation-id="Root"/>)
-
+        properties((p: Props) => <div data-automation-id="Root"/>)
+    });
+});
