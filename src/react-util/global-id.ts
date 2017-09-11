d48b5b84b682e0701b3ea3d242b7fb642410601b
Lior Zisman <liorzi@wix.com>
2017-09-11 15:54:09 +0300

    added id case

--- src/react-util/global-id.ts
+++ src/react-util/global-id.ts
@@ -1,3 +1,14 @@
-export function isClassInstance(obj: object): boolean {
-    return true;
+import * as React from 'react';
+
+export function isComponentInstance(value: any): value is React.Component {
+    return value && value instanceof React.Component;
+}
+
+export function getRootId(obj: object): string {
+    if (isComponentInstance(obj)) {
+        if (obj.props.hasOwnProperty('id')) return (obj.props as {id: string}).id;
+    } else {
+
+    }
+    return 'abc';
 }
--- test/react-util/global-id.spec.ts
+++ test/react-util/global-id.spec.ts
@@ -1,28 +1,33 @@
 import * as React from 'react';
 import { expect } from 'test-drive-react';
+import { getRootId } from '../../src/react-util/global-id';
+
+interface TestProps {
+    id: string;
+}
+
+class TestClass extends React.Component<TestProps> {}
 
 describe('GlobalID', () => {
    describe('getRootId', () => {
+        it('resolves the id from props in case it was passed', () => {
+            const id = 'pancake';
+            const testClass = new TestClass({id});
 
+            expect(getRootId(testClass)).to.equal(id);
+        });
+
+        it('resolves the id from a component\'s private state (if relevant) if it was not provided in props', () => {
+            throw new Error('To be implemented');
+        });
+        it('produces a new unique ID and stores it in case there was no id given in props or found in private state', () => {
+            throw new Error('To be implemented');
+        });
    });
 
    describe('getLocalId', () => {
-
-   });
-
-   describe('Helper methods', () => {
-
-       class TestClass extends React.Component {}
-
-       describe('isClassInstance', () => {
-            it('detects that an object is an instance of a component class', () => {
-                expect(true).to.be.ok;
-            });
-
-            it('detects that an object is not an instance of a component class', () => {
-
-            });
-       })
-
+       it('generates a new unique ID for a child element', () => {
+           throw new Error('To be implemented');
+       });
    });
 });
