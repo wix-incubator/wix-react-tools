156217e38cd4acc5bf173c0250738442b2ca1923
Lior Zisman <liorzi@wix.com>
2017-09-12 11:42:40 +0300

    cleanup

--- src/react-util/global-id.ts
+++ src/react-util/global-id.ts
@@ -8,16 +8,17 @@ export function isComponentInstance(value: any): value is React.Component {
 }
 
 let counter: number = 0;
-const provider: StateProvider<number, object> = privateState('globalId', () => counter++);
+const provider: StateProvider<number, object> = privateState('globalId', () => ({ id: counter++ }));
 
 export function getRootId(obj: object): string {
+    debugger;
     if (isComponentInstance(obj)) {
-        if (obj.props.hasOwnProperty('id')) return (obj.props as {id: string}).id;
+        if (obj.props && obj.props.hasOwnProperty('id')) return (obj.props as {id: string}).id;
     } else {
         if (obj.hasOwnProperty('id')) return (obj as {id: string}).id;
     }
 
-    return `${provider(obj)}`;
+    return `${provider(obj).id}`;
 }
 
 export function getLocalId(rootId: string, id: string): string {
--- test/react-util/global-id.spec.ts
+++ test/react-util/global-id.spec.ts
@@ -10,18 +10,26 @@ class TestClass extends React.Component<TestProps> {}
 
 describe('GlobalID', () => {
    describe('getRootId', () => {
+       const id = 'pancake';
+
         it('resolves the id from props in case it was passed', () => {
-            const id = 'pancake';
             const testClass = new TestClass({id});
 
             expect(getRootId(testClass)).to.equal(id);
         });
 
         it('resolves the id from a component\'s private state (if relevant) if it was not provided in props', () => {
-            const testClass = new TestClass();
+            const firstInstance = new TestClass();
+            expect(getRootId(firstInstance)).to.equal('0');
 
-            expect(getRootId(testClass)).to.equal(0);
+            const providedIdInstance = new TestClass({id});
+            expect(getRootId(providedIdInstance)).to.equal(id);
+
+            const incrementedIdInstance = new TestClass();
+            expect(getRootId(incrementedIdInstance)).to.equal('1');
+            expect(getRootId(firstInstance)).to.equal('0');
         });
+
         it('produces a new unique ID and stores it in case there was no id given in props or found in private state', () => {
             throw new Error('To be implemented');
         });
