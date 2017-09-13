475cd21d4c992f74a2a8ca7531776bcd401fd408
Lior Zisman <liorzi@wix.com>
2017-09-13 12:01:22 +0300

    testing most cases

--- typings-checker/global-id.tsx
+++ typings-checker/global-id.tsx
@@ -9,5 +9,32 @@ describe('Global ID', () => {
 // $ExpectError tried to get root id for a props object
             getRootId({});
         });
+
+        it('throws an error when passed something other than object', () => {
+// $ExpectError tried to get root id for a props object
+            getRootId([]);
+
+// $ExpectError Argument of type '"wrong"' is not assignable
+            getRootId('wrong');
+
+// $ExpectError Argument of type 'null' is not assignable
+            getRootId(null);
+        });
+    });
+
+    describe('getLocalId', () => {
+        it('throws an error when passed something other than strings', () => {
+// $ExpectError Argument of type '{}' is not assignable
+            getLocalId({}, 'right');
+
+// $ExpectError Argument of type '{}' is not assignable
+            getLocalId('right', {});
+
+// $ExpectError Argument of type 'null' is not assignable
+            getLocalId(null, 'right');
+
+// $ExpectError Argument of type 'null' is not assignable
+            getLocalId('right', null);
+        });
     });
 });
