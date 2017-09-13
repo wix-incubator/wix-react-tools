a10ead7ad1526c13dc1489f524c8862f6d1fe113
Lior Zisman <liorzi@wix.com>
2017-09-13 10:54:21 +0300

    global id types

---
+++ typings-checker/global-id.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { globalId } from "../dist/src/index";
+
+describe(`with porperties type that includes properties.Props`, () => {
+    it(`can decorate class components`, () => {
+        @properties
+        class Comp extends React.Component<VerboseProps> {
+            render() {
+                return <div data-automation-id="Root"/>;
+            }
+        }
+    });
+
+    it(`can decorate functional components`, () => {
+        properties((p: VerboseProps) => <div data-automation-id="Root"/>)
+    });
+});
+
+describe(`with porperties type that does not include properties.Props`, () => {
+    it(`error decorating class components`, () => {
+// $ExpectError 'typeof Comp2' is not assignable to
+        @properties
+        class Comp2 extends React.Component<Props> {
+            render() {
+                return <div data-automation-id="Root"/>;
+            }
+        }
+
+    });
+
+    it(`error wrapping SFC functional components`, () => {
+// $ExpectError '(p: Props) => Element' is not assignable to
+        properties((p: Props) => <div data-automation-id="Root"/>)
+    });
+});
