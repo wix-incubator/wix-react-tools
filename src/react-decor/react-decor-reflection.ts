5665620a9cd2055a9e31e52091130a6c4e024f5a
amir-arad <amira@wix.com>
2017-10-04 11:35:20 +0300

    removed obselete test add getDecortated reflection add failing test for not double-wrapping components

--- src/react-decor/react-decor-reflection.ts
+++ src/react-decor/react-decor-reflection.ts
@@ -4,35 +4,54 @@ declare const process: {env : {[k:string]: any}};
 
 export const decorationReflection = reflection('react-decor-reflection');
 
+interface Metadata {
+    decorators : any[];
+    original : object;
+}
 export function reflection<T extends object = any>(id: string) {
-    const decorationReflection = privateState<Array<any>, T>(id, (targetObj: T) => []);
+    const decorationReflection = privateState<Metadata, T>(id, (targetObj: T) => ({
+        original : null as any,
+        decorators : []
+    }));
 
     return {
         registerDecorator(source: T, target: T, decoratorId: any): void {
             const isTargetDecorated = decorationReflection.hasState(target);
-            const newDecorators = decorationReflection(target); // new private state will be created if missing
+            const metadata = decorationReflection(target); // new private state will be created if missing
 
-            // check if target is not yet decorated, and copies all decorators from the source to the new target
-            if (!isTargetDecorated && source !== target && decorationReflection.hasState(source)) {
-                const prevDecorators = decorationReflection(source);
-                newDecorators.push(...prevDecorators);
+            if (decorationReflection.hasState(source)){
+                console.error('TODO: no double wrappings!');
             }
-
-            if (newDecorators.indexOf(decoratorId) !== -1) {
+            if (!isTargetDecorated) {
+                metadata.original = source;
+                // check if target is not yet decorated, and copies all decorators from the source to the new target
+                if (source !== target && decorationReflection.hasState(source)) {
+                    const prevMetadata = decorationReflection(source);
+                    metadata.decorators.push(...prevMetadata.decorators);
+                }
+            }
+            if (metadata.decorators.indexOf(decoratorId) !== -1) {
                 if (process.env.NODE_ENV !== 'production') {
                     console.warn(target, ' is already decorated with ', decoratorId);
                 }
                 return undefined;
             }
-            newDecorators.push(decoratorId);
+            metadata.decorators.push(decoratorId);
         },
         isDecorated(Comp: T, decoratorId?: any): boolean {
             const decorators = decorationReflection(Comp);
             if (!decoratorId) {
-                return decorationReflection(Comp).length > 0;
+                return decorationReflection(Comp).decorators.length > 0;
             } else {
-                return decorators.some(decorator => decorator === decoratorId);
+                return decorators.decorators.some(decorator => decorator === decoratorId);
             }
+        },
+        getDecorated(Comp: T): T | null {
+            if (decorationReflection.hasState(Comp)){
+                return decorationReflection(Comp).original as T;
+            }
+            return null;
         }
+
     }
 }
