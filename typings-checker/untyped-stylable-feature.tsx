5c6cc567fb536310fac4b91851f25db5f524cb77
amir-arad <amira@wix.com>
2017-10-23 15:14:14 +0300

    regression for stylable typings test when user does not import stylesheet

---
+++ typings-checker/untyped-stylable-feature.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import {stylable} from "../dist/src/index";
+
+interface CompProps {
+}
+
+// assume stylesheet is not typed
+declare const style: any;
+
+// $ExpectType (props: any) => Element
+stylable(style)(props => {
+    return <div/>;
+});
+
+// $ExpectType StatelessComponent<CompProps>
+stylable(style)<React.SFC<CompProps>>(props => {
+    return <div/>;
+});
+
+// $ExpectType (props: CompProps) => Element
+stylable(style)((props: CompProps) => {
+    return <div/>;
+});
